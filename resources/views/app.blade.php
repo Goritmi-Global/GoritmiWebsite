c221dcf1f55221cb30bf992fd019f3a24fb64e7e
BILAL HUSSAIN <billhssn777@icloud.com>
2025-03-05 16:00:47 +0500

    updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
      
-    <link rel="icon" href="asset('/images/fav.png')" type="image/png">
+     
+    <link rel="icon" href="{{ asset('images/fav.png') }}" type="image/png">
+
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
